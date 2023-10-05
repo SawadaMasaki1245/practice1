b53add66a281314cbf0b72ae06e87400574dcec7
sawadamasaki <m.sawada.sys23@morijyobi.ac.jp>
2023-10-05 10:50:18 +0900

    pra2.javaに出力処理追記

--- pra2.java
+++ pra2.java
@@ -1,6 +1,6 @@
-<!doctype html>
-<java lang="ja">
-<body>
-Hello World
-</body>
-</html>
+public class pra2{
+	public static void main(String[] args) {
+		for(int i=0; i<101;i++){
+			System.out.println("Hello World");}
+	}
+}
